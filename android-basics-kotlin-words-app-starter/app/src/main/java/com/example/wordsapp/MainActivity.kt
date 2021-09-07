ca9d0de0b36810930c1742780ee4c02ca220860f
tausif <tausiffardin4@gmail.com>
2021-09-07 11:32:36 +0600

    Added all apps

---
+++ android-basics-kotlin-words-app-starter/app/src/main/java/com/example/wordsapp/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.wordsapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.wordsapp.databinding.ActivityMainBinding
+
+/**
+ * Main Activity and entry point for the app. Displays a RecyclerView of letters.
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
---
+++ android-basics-kotlin-words-app-starter/app/src/main/java/com/example/wordsapp/WordListFragment.kt
@@ -0,0 +1,18 @@
+package com.example.wordsapp
+
+import androidx.fragment.app.Fragment
+import com.example.wordsapp.databinding.FragmentWordListBinding
+
+class WordListFragment : Fragment() {
+    companion object {
+        const val LETTER = "letter"
+        val WordListFragment = "https://www.google.com/search?q="
+
+
+
+    }
+    private var _binding: FragmentWordListBinding? = null
+    private val binding get() = _binding!!
+
+
+}
